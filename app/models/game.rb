81900b5ec88239bee8dfa9188c0ceb3695737d22
thirdchance57 <thirdchance57@hotmail.com>
2014-12-02 17:29:23 -0800

    removing games_controller/model, platforms_controller/model

--- app/models/game.rb
+++
@@ -1,6 +0,0 @@
-class Game
-  include Mongoid::Document
-  field :madden, type: String
-  field :cod, type: String
-  field :battlefield4, type: String
-end
--- app/models/platform.rb
+++
@@ -1,8 +0,0 @@
-class Platform
-  include Mongoid::Document
-  field :ps, type: String
-  field :xbox, type: String
-  field :pc, type: String
-  field :ios, type: String
-  field :andro, type: String
-end
